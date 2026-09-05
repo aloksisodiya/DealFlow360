6aadb6adaa61a8d5f733c89bf44b38893eee83b2
Arjav Dariya <arjavdariya2@gmail.com>
2026-09-05 11:06:38 +0530

    fix(vite): configure server host and port

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Listen on all local IPs including 127.0.0.1 and localhost
+    port: 5173,
+    strictPort: true
+  }
 })
